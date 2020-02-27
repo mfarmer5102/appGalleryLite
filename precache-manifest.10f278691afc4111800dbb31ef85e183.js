e82b967d59462597cd6ded23eaff2e9d099296fb
Matthew <matthew-dev@Matthews-MacBook-Air.local>
2020-02-26 21:58:55 -0500

    Updates

---
+++ precache-manifest.10f278691afc4111800dbb31ef85e183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac946c2a09ad15564769695be1672ec",
+    "url": "/appGalleryLite/index.html"
+  },
+  {
+    "revision": "47704222dcec8f34d9f0",
+    "url": "/appGalleryLite/static/js/2.52511dd6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/appGalleryLite/static/js/2.52511dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262da6d873c8af2a5f27",
+    "url": "/appGalleryLite/static/js/main.f5a79a37.chunk.js"
+  },
+  {
+    "revision": "97f377bf19b131d10dfe",
+    "url": "/appGalleryLite/static/js/runtime-main.6e3e217c.js"
+  },
+  {
+    "revision": "1321cdd63e6cbe61c7f28474b994e4b7",
+    "url": "/appGalleryLite/static/media/banner.1321cdd6.jpg"
+  }
+]);
